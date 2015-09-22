c1f732fc7e9ccd155e964b3c0df3411bbb7014ce
Lafunamor <info@zipfuzapfu.ch>
2015-09-22 17:41:18 +0200

    added missing files fixed db issue

--- db/migrate/20150922094139_create_users.rb
+++ db/migrate/20150922094139_create_users.rb
@@ -2,9 +2,11 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.text :username
-      t.text :password
 
       t.timestamps
     end
+  end
+  def change
+    add_index :users, :username, unique: true
   end
 end
