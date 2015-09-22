a7ba893267c61701e115a0feae82145e9d6876a6
Lafunamor <info@zipfuzapfu.ch>
2015-09-22 16:49:32 +0200

    added dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :bikes
-  has_many :bids
+  has_many :bikes, dependent: :destroy
+  has_many :bids, dependent: :destroy
 end
