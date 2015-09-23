3c6fcb7c200ab38102439ea181943782b21f0301
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-09-23 16:57:56 +0200

    created auction controller, views and some function.

--- app/controllers/auction_controller.rb
+++ app/controllers/auction_controller.rb
@@ -1,2 +1,54 @@
 class AuctionController < ApplicationController
+
+  def index
+    @bike = Bike.find(params[:bike_id])
+    @bikes = [@bike]
+  end
+
+  def show
+    @bike = Bike.find(params[:bike_id])
+    @auction = @bike.auction
+  end
+
+  def new
+    @bike = Bike.find(params[:bike_id])
+    @auction = @bike.build_auction
+  end
+
+  def create
+    @bike = Bike.find(params[:bike_id])
+    @auction = @bike.create_auction(auction_params)
+    @bike.save
+
+    redirect_to bike_auction_path(@bike, @auction)
+  end
+
+  def edit
+    @bike = Bike.find(params[:bike_id])
+    @auction = @bike.auction
+  end
+
+  def update
+    @bike = Bike.find(params[:bike_id])
+    @auction = @bike.auction
+    if @auction.update(auction_params)
+      redirect_to @auction
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @bike = Bike.find(params[:bike_id])
+    @auction = @bike.auction
+    @auction.destroy
+
+    redirect_to bikes_path
+  end
+
+  private
+  def auction_params
+    params.require(:auction).permit(:enddate)
+  end
+
 end
