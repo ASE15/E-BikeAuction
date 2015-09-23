69804f4359267dd4f524b2683bb7af039567d4db
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-09-23 11:58:28 +0200

    added simple seach

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -3,4 +3,25 @@ class Bike < ActiveRecord::Base
   has_one :auction
   has_many :pictures, :inverse_of => :bike, :dependent => :destroy
   accepts_nested_attributes_for :pictures, allow_destroy: true
+
+  def self.search(query)
+    where("name LIKE ? OR brand LIKE ? OR color LIKE ? ", "%#{query}%", "%#{query}%", "%#{query}%")
+  end
+
+  def self.search_each(query)
+
+    @bikes = self.search(query).all
+
+    keywords = query.split(" ")
+    keywords.each do |q|
+      bikes_tmp = self.search(q).all
+      if not bikes_tmp.empty?
+        for b in bikes_tmp
+          @bikes << b
+        end
+      end
+    end
+    @bikes = @bikes.flatten.uniq(&:id)
+    return @bikes
+  end
 end
