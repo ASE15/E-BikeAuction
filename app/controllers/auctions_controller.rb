5c5eea6048ed252a9a9fa94a9f9591bd077ece33
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-09-24 19:16:25 +0200

    implemented search in auctions list

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,6 +1,19 @@
 class AuctionsController < ApplicationController
   def index
-    @auctions = Auction.all
+
+    if params[:search] and not params[:search].empty?
+
+      #@bikes = Bike.search_each(params[:search])
+      @auctions = Array[]
+      @bikes = Bike.search_each(params[:search])
+      @bikes.each do |b|
+        if not b.auction.nil?
+          @auctions.push(b.auction)
+        end
+      end
+    else
+      @auctions = Auction.all
+    end
   end
 
   def show
