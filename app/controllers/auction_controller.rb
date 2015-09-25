daaba707b7e67122ab38d2376d4730e42961f397
Andreas Hohler (ah12z468) <andreas.hohler@students.unibe.ch>
2015-09-25 11:12:25 +0200

    reordered auction methods and views. auctions_controller is in charge vor index view, show, update, destroy. auction_controller is in charge for creation.

--- app/controllers/auction_controller.rb
+++ app/controllers/auction_controller.rb
@@ -1,14 +1,7 @@
 class AuctionController < ApplicationController
 
-  def index
-    @bike = Bike.find(params[:bike_id])
-    @bikes = [@bike]
-  end
-
-  def show
-    @bike = Bike.find(params[:bike_id])
-    @auction = @bike.auction
-  end
+  before_action :authenticate_user!
+  before_filter :owns_bike!, :only => [:new, :create]
 
   def new
     @bike = Bike.find(params[:bike_id])
@@ -18,34 +11,17 @@ class AuctionController < ApplicationController
   def create
     @bike = Bike.find(params[:bike_id])
     @auction = @bike.create_auction(auction_params)
-    if @auction.save
-      redirect_to bike_auction_path(@bike, @auction)
+
+    now = DateTime.now
+    now = now.change(:offset => "+0000")
+    if @auction.endtime < now
+      flash[:error] = 'Endtime must be in the future'
+      render 'new'
+    elsif @auction.save
+      redirect_to auction_path(@auction)
     else
       render 'new'
     end
-  end
-
-  def edit
-    @bike = Bike.find(params[:bike_id])
-    @auction = @bike.auction
-  end
-
-  def update
-    @bike = Bike.find(params[:bike_id])
-    @auction = @bike.auction
-    if @auction.update(auction_params)
-      redirect_to @auction
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @bike = Bike.find(params[:bike_id])
-    @auction = @bike.auction
-    @auction.destroy
-
-    redirect_to bikes_path
   end
 
   private
--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,4 +1,7 @@
 class AuctionsController < ApplicationController
+
+  before_filter :owns_auction!, :only => [:update, :edit, :destroy]
+
   def index
 
     if params[:search] and not params[:search].empty?
@@ -12,12 +15,54 @@ class AuctionsController < ApplicationController
         end
       end
     else
-      @auctions = Auction.all
+      @auctions = Auction.all.to_a
     end
   end
 
   def show
     @auction = Auction.find(params[:id])
     @bike = @auction.bike
+  end
+
+
+  def edit
+    #@bike = Bike.find(params[:bike_id])
+    #@auction = @bike.auction
+    @auction = Auction.find(params[:id])
+
+    if @auction.bids.count > 0
+      flash[:error] = 'The auction already has bids! You cannot edit it.'
+      redirect_to auction_path(@auction)
+    end
+  end
+
+  def update
+    #@bike = Bike.find(params[:bike_id])
+    @auction = Auction.find(params[:id])
+    if @auction.update(auction_params)
+      redirect_to auction_path(@auction)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    #@bike = Bike.find(params[:bike_id])
+    @auction = Auction.find(params[:id])
+
+    if @auction.bids.count > 0
+      flash[:error] = 'The auction already has bids! You cannot delete it.'
+      redirect_to auction_path(@auction)
+    else
+
+      @auction.destroy
+
+      redirect_to bikes_path
+    end
+  end
+
+  private
+  def auction_params
+    params.require(:auction).permit(:endtime, :current_amount)
   end
 end
